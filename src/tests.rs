b6a294e5b2bb7d2bd63b38cdc2b376d4acf3f524
William Venner <william@venner.io>
2022-06-05 19:33:59 +0100

    Add string interpolation injection test and fix curl with -g flag

--- src/tests.rs
+++ src/tests.rs
@@ -1,7 +1,8 @@
+#[cfg(test)]
+use crate::SystemHTTPClient;
+
 #[test]
 fn test_get() {
-	use crate::SystemHTTPClient;
-
 	let reqwest = reqwest::blocking::Client::new();
 
 	for test_url in ["https://www.google.com/favicon.ico", "http://www.example.org"] {
@@ -31,5 +32,34 @@ fn test_naughty_url() {
 		Ok(_) => panic!("pwned"),
 		Err(super::Error::InvalidUrlScheme) => {},
 		Err(err) => panic!("{err}")
+	}
+}
+
+#[test]
+fn test_str_interp_url() {
+	std::env::set_var("SYSREQ_PWNED", "http://example.org");
+
+	for client in crate::clients::all_http_clients() {
+		for interp in ["$SYSREQ_PWNED", "`SYSREQ_PWNED`", "${SYSREQ_PWNED}", "[[SYSREQ_PWNED]]"].into_iter() {
+			if let Ok(result) = client.get(interp) {
+				if !result.is_empty() {
+					panic!("This should have failed: {}", String::from_utf8_lossy(&result));
+				}
+			}
+		}
+
+		if let Ok(result) = client.get("#//\"\"\"\"\"'''''[[]]`````${hello}$hello###") {
+			if !result.is_empty() {
+				panic!("This should have failed: {}", String::from_utf8_lossy(&result));
+			}
+		}
+
+		let example = client.get("http://example.org").unwrap();
+		let result = client.get("http://example.org/#//\"\"\"\"\"'''''[[]]`````${hello}$hello###").unwrap();
+		if example != result {
+			let example = String::from_utf8_lossy(&example);
+			let result = String::from_utf8_lossy(&result);
+			panic!("Diff:\n{}", difference::Changeset::new(example.as_ref(), result.as_ref(), ""));
+		}
 	}
 }
